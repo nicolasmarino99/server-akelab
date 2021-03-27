e74cba8ac8d4fc3a20b2805a69780c67fcea0595
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-03-26 21:42:38 -0500

    add routes, add controllers

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-import cors from 'cors'
 import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
+import cors from 'cors'
+import postRoutes from "./routes/posts.js";
 
 const app = express()
 
-app.use(express.json({ limit: '30mb', extended: true}))
-app.use(express.urlencoded({ limit: '30mb', extended: true}))
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
+app.use(cors())
 
 const CONNECTION_URL = 'mongodb+srv://akelab:mipapamemima99@cluster0.2wq6c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> app.listen(PORT, () => console.log(`server running on host ${PORT}`)))
+.catch((error) => console.log(`${error} did nor connect`) )
+
+mongoose.set('useFindAndModify', false)
---
+++ routes/posts.js
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import { getPosts } from "../controllers/posts.js";
+
+const router = Router()
+
+router.get('/', getPosts)
+
+export default router;
